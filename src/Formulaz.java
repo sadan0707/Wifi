0ae64dd44ab63be24b46a1708fa88e8a94c34e95
sadan0707 <sadan0707@gmail.com>
2014-10-22 08:10:38 +0200

    czesc 3

--- src/Formulaz.java
+++ src/Formulaz.java
@@ -1,8 +1,10 @@
 import java.awt.Component;
+import java.awt.Font;
 import java.awt.Frame;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
@@ -15,8 +17,8 @@ public class Formulaz {
 		JFrame zgloszenie = new JFrame("Formulaz zgloszenia");
 		
 		zgloszenie.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		zgloszenie.setSize(600,400);	
-		zgloszenie.setLocation(300, 300);
+		zgloszenie.setSize(700,1000);	
+		zgloszenie.setLocation(300, 100);
 		zgloszenie.setLayout(null);		// zajmuje calosc	
 		zgloszenie.setResizable(false); // mozliwosc zmiany rozmiaru okna
 		zgloszenie.setVisible(true);   //za pomoca tej metody mozemy zauwa¿yc okno
@@ -25,14 +27,69 @@ public class Formulaz {
 		//zgloszenie.add(panel);
 		
 		
-		JButton wystaw = new JButton("WYSTAW");
-		wystaw.setBounds(490, 10, 100, 40);
+		JButton wystaw = new JButton("WYSTAW ZGLOSZENIE");
+		wystaw.setBounds(250, 920, 200, 40);
 		wystaw.setBorder(null);
 		zgloszenie.add(wystaw);
 		
+		JLabel etykieta_nazwy = new JLabel("Nazwa urzadzenia");
+		etykieta_nazwy.setBounds(20,10,120,40);
+		zgloszenie.add(etykieta_nazwy);
+		
+		
 		JTextField nazwa = new JTextField();
-		nazwa.setBounds(10, 10, 200, 20);
+		nazwa.setBounds(20, 50, 650, 40);
+		Font f = new Font("New Times Roman", Font.BOLD, 20);
+		nazwa.setFont(f);
+		nazwa.setHorizontalAlignment(JTextField.RIGHT);
 		zgloszenie.add(nazwa);
+		
+		JLabel etykieta_modelu = new JLabel("Model");
+		etykieta_modelu.setBounds(20,110,120,40);
+		zgloszenie.add(etykieta_modelu);
+		
+		
+		JTextField model = new JTextField();
+		model.setBounds(20, 150, 650, 40);
+		model.setFont(f);
+		model.setHorizontalAlignment(JTextField.RIGHT);
+		zgloszenie.add(model);
+		
+		/*JTextField nazwa = new JTextField();
+		nazwa.setBounds(10, 10, 500, 40);
+		Font f = new Font("New Times Roman", Font.BOLD, 24);
+		nazwa.setFont(f);
+		nazwa.setHorizontalAlignment(JTextField.RIGHT);
+		zgloszenie.add(nazwa);
+		
+		JTextField nazwa = new JTextField();
+		nazwa.setBounds(10, 10, 500, 40);
+		Font f = new Font("New Times Roman", Font.BOLD, 24);
+		nazwa.setFont(f);
+		nazwa.setHorizontalAlignment(JTextField.RIGHT);
+		zgloszenie.add(nazwa);
+		
+		JTextField nazwa = new JTextField();
+		nazwa.setBounds(10, 10, 500, 40);
+		Font f = new Font("New Times Roman", Font.BOLD, 24);
+		nazwa.setFont(f);
+		nazwa.setHorizontalAlignment(JTextField.RIGHT);
+		zgloszenie.add(nazwa);
+		
+		JTextField nazwa = new JTextField();
+		nazwa.setBounds(10, 10, 500, 40);
+		Font f = new Font("New Times Roman", Font.BOLD, 24);
+		nazwa.setFont(f);
+		nazwa.setHorizontalAlignment(JTextField.RIGHT);
+		zgloszenie.add(nazwa);
+		
+		JTextField nazwa = new JTextField();
+		nazwa.setBounds(10, 10, 500, 40);
+		Font f = new Font("New Times Roman", Font.BOLD, 24);
+		nazwa.setFont(f);
+		nazwa.setHorizontalAlignment(JTextField.RIGHT);
+		zgloszenie.add(nazwa); */
+		
 		
 		
 		
